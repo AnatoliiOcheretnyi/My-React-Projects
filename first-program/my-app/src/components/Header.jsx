c28e1cbfcb47781aa895bf18d42999109207271d
Anatolii <anatoliy.ocheretnyi@gmail.com>
2019-08-09 01:52:41 +0300

    start to learn about react

---
+++ first-program/my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return (
+        <header className='header'>
+         <img src='http://pluspng.com/img-png/png-logo-design-design-png-hd-1250.png' />
+       </header>
+    );
+}
+
+export default Header;
