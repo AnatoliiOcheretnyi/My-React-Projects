3aaad4477627a6ef36fdcebd22c8a51bc3a80cea
Anatolii <anatoliy.ocheretnyi@gmail.com>
2019-08-12 23:39:22 +0300

    new components

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import classes from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import DialogsItem from "./DialogsItem/DialogsItem";
+import MessageItem from "./MessageItem/MessageItem";
 
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={`${classes.dialog} + ${classes.activeDialog}`}>
-            <NavLink to={path}>
-                {props.name}
-            </NavLink>
 
-        </div>
-    );
-}
 
-const MessageItem = (props) => {
-    return (
-        <div className={classes.message}>
-            {props.message}
-        </div>
-    );
-}
+
+
 
 const Dialogs = (props) => {
 
@@ -34,7 +20,7 @@ const Dialogs = (props) => {
         {id: 2, message: 'How are you'}
     ]
 
-    let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let dialogsElement = dialogs.map(d => <DialogsItem name={d.name} id={d.id}/>)
     let messagesElement = messages.map(m => <MessageItem message={m.message}/>)
 
     return (
