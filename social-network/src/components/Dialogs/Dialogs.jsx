d250b6a9bf614e82dceb7b74f8dc475777e2428a
Anatolii <anatoliy.ocheretnyi@gmail.com>
2019-08-12 17:04:19 +0300

    refactoring dialogs and myposts with  map

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -2,36 +2,48 @@ import React from 'react';
 import classes from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+        <div className={`${classes.dialog} + ${classes.activeDialog}`}>
+            <NavLink to={path}>
+                {props.name}
+            </NavLink>
+
+        </div>
+    );
+}
+
+const MessageItem = (props) => {
+    return (
+        <div className={classes.message}>
+            {props.message}
+        </div>
+    );
+}
+
 const Dialogs = (props) => {
+
+    let dialogs = [
+        {id: 1, name: 'Anatolii'},
+        {id: 2, name: 'Anastasia'}
+    ]
+
+    let messages = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you'}
+    ]
+
+    let dialogsElement = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElement = messages.map(m => <MessageItem message={m.message}/>)
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/1'>
-                        Anatolii
-                    </NavLink>
-                </div>
-                <div className={`${classes.dialog} + ${classes.activeDialog}`}>
-                    <NavLink to='/dialogs/2'>
-                        Anstasia
-                    </NavLink>
-
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>
-                        Svitlana
-                    </NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/4'>
-                        Bogdan
-                    </NavLink>
-                </div>
+                {dialogsElement}
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi</div>
-                <div className={classes.message}>How are you</div>
-                <div className={classes.message}>it is great</div>
+                {messagesElement}
             </div>
         </div>
     );
--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,30 @@ import classes from './MyPosts.module.css'
 import Post from './Posts/Post';
 
 const MyPosts = () => {
-  return (
-    <div>
-      <textarea></textarea>
-      <button>Add post</button>
-      <div>
-        <Post counter ='2' message='hi, how are you' />
-        <Post counter = '3' message="it's my new post" />
-      </div>
-    </div>
-  );
+
+    let posts = [
+        {id: 1, message: 'hi, how are you', counter: 21},
+        {id: 2, message: 'it\'s my new post', counter: 35}
+    ]
+
+    let postsElement = posts.map(p => <Post counter={p.counter} message={p.message} />)
+
+    return (
+        <div className={classes.myPostsBlock}>
+            <h2>My Posts</h2>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
+            </div>
+            <div className={classes.posts}>
+                {postsElement}
+            </div>
+        </div>
+    );
 }
 
 export default MyPosts;
